58cbdcd28d4705a60da9381e285722dbe4351fb3
ShiuChauNeng <super0818@hotmail.com>
2020-05-09 14:29:48 +0800

    Super tool initial

---
+++ SuperTool/SuperTool/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
