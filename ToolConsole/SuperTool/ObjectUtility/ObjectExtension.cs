3449a8a63d5aae4c3602a029742bb55695cca3f2
ShiuChauNeng <super0818@hotmail.com>
2020-05-09 14:57:31 +0800

    加入 object、Property Tool

---
+++ ToolConsole/SuperTool/ObjectUtility/ObjectExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperTool.ObjectUtility
+{
+    public static class ObjectExtension
+    {
+        public static void SetNullStringPropertyToEmpty(this object content)
+        {
+            Type type = content.GetType();
+            if (type.IsClass)
+            {
+                var stringProperties = type.GetProperties().Where(o => o.PropertyType == typeof(string));
+                foreach (PropertyInfo propertyInfo in stringProperties)
+                {
+                    if (propertyInfo.GetValue(content) == null)
+                    {
+                        propertyInfo.SetValue(content, string.Empty);
+                    }
+                }
+            }
+        }
+
+        public static bool IsNull(this object obj)
+        {
+            return obj == null;
+        }
+    }
+}
---
+++ ToolConsole/SuperTool/PropertyUtility/PropertyTool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperTool.PropertyUtility
+{
+    public class PropertyTool
+    {
+        public static void SetNullStringPropertyToEmpty<T>(T t) where T : class
+        {
+            Type type = typeof(T);
+            var stringProperties = type.GetProperties().Where(o => o.PropertyType == typeof(string));
+            foreach (PropertyInfo propertyInfo in stringProperties)
+            {
+                if (propertyInfo.GetValue(t) == null)
+                {
+                    propertyInfo.SetValue(t, string.Empty);
+                }
+            }
+        }
+    }
+}
